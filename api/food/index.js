6013b0abf1404ba12124defdd5f484df4bd7b1e1
박민재 <sobu0715@gmail.com>
2018-12-09 02:37:29 +0900

    #3 Food Commit

--- api/food/index.js
+++ api/food/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 const create = require('./create')
 const remove = require('./remove')
 const search = require('./search')
+const lookup = require('./lookup')
 const upload = require('../../util/multer')
 
 router.post('/create', upload.single('image'), create.FoodCreate)
 router.post('/remove', remove.FoodRemove)
 router.get('/search', search.FoodSearch)
+router.post('/lookup', lookup.FoodLookup)
 
 module.exports = router
