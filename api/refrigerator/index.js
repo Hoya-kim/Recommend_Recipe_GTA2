6c418c9697d47c324013418cbe0fbae8f1077c43
박민재 <sobu0715@gmail.com>
2018-12-10 01:42:01 +0900

    #2 Refrigerator Commit

--- api/refrigerator/index.js
+++ api/refrigerator/index.js
@@ -5,9 +5,13 @@ const router = express.Router()
 const insert = require('./insert')
 const takeout = require('./takeout')
 const open = require('./open')
+const getFood = require('./getFood')
+const getMoreFood = require('./getMoreFood')
 
 router.post('/insert', insert.Insert)
 router.post('/takeout', takeout.Takeout)
 router.post('/open', open.Open)
+router.post('/getFood', getFood.getFood)
+router.post('/getMoreFood', getMoreFood.getMoreFood)
 
 module.exports = router
